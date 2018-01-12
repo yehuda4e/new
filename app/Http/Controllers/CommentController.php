d18864a8ede383025db75d7207fe093092e5ede4
Yehuda Shamayev <yehuda4e@gmail.com>
2018-01-12 22:35:25 +0200

    add middleware protection

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,6 +6,11 @@ use App\Comment;
 
 class CommentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function like(Comment $comment)
     {
         $comment->like();
