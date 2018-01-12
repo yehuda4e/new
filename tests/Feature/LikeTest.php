ca8270dfa0c644ab70b0072684dc8c4d0f31ce34
Yehuda Shamayev <yehuda4e@gmail.com>
2018-01-12 22:32:18 +0200

    improve the test

--- tests/Feature/LikeTest.php
+++ tests/Feature/LikeTest.php
@@ -11,9 +11,50 @@ class LikeTest extends TestCase
     use  RefreshDatabase;
 
     /** @test */
-    public function guests_can_not_like_thing()
+    public function guests_can_not_like_comments()
     {
-        $this->post('/comment/1/likde')
+        $this->get('/comment/1/like')
+            ->assertRedirect('/login');
+
+        $this->get('/comment/1/unlike')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function user_can_only_like_once()
+    {
+        $user = factory('App\User')->create();
+        $this->be($user);
+
+        $comment = factory('App\Comment')->create(['commentable_type' => 'App\Comment']);
+
+        $comment->like();
+        $comment->like();
+
+        $this->assertCount(1, $comment->likes);
+    }
+
+    /** @test */
+    public function user_can_unlike()
+    {
+        $user = factory('App\User')->create();
+        $this->be($user);
+
+        $comment = factory('App\Comment')->create(['commentable_type' => 'App\Comment']);
+
+        $comment->like();
+        $comment->unlike();
+        
+        $this->assertCount(0, $comment->likes);
+    }
+
+    /** @test */
+    public function guests_can_not_like_topics()
+    {
+        $this->get('topic/slug/like')
+            ->assertRedirect('/login');
+
+        $this->get('topic/slug/unlike')
             ->assertRedirect('/login');
     }
 }
